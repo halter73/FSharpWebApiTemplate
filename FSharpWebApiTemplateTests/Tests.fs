c48e6a4af057f110ad8cf3c42e723e887708d323
Stephen Halter <halter73@gmail.com>
2021-09-17 15:25:32 -0700

    Add WebApplicationFactory test

--- FSharpWebApiTemplateTests/Tests.fs
+++ FSharpWebApiTemplateTests/Tests.fs
@@ -1,8 +1,20 @@
 module Tests
 
 open System
+open System.Net.Http.Json
+open FSharpWebApiTemplate
+open Microsoft.AspNetCore.Mvc.Testing
 open Xunit
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``WebApplicationFactory test`` () =
+    async {
+        let webApp = new WebApplicationFactory<WeatherForecast>()
+        let client = webApp.CreateDefaultClient()
+        let! forecast = client.GetFromJsonAsync<WeatherForecast[]>("/weatherforecast") |> Async.AwaitTask
+        Assert.Equal(5, forecast.Length)
+    }
